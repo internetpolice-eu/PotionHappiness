2b43eaae6ddbd7486656b3e6ea3c1128973433b5
Muspah <simondw@sapo.pt>
2019-12-31 11:12:33 +0000

    Initial commit

---
+++ src/main/java/eu/internetpolice/PotionHappiness.java
@@ -0,0 +1,15 @@
+package eu.internetpolice;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PotionHappiness extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
